de64c715cf79a589a47452d4eae08cb94b872242
J3eff <jeffersonvideo125@gmail.com>
2025-06-17 16:24:34 -0300

    Criando o Menu

---
+++ src/components/Header/components/HeaderList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../Header.module.css'
+
+
+function HeaderList({ children }: React.HTMLAttributes<HTMLUListElement>) {
+  return (
+    <ul className={styles.cabecalho}>{children}</ul>
+  )
+}
+
+export default HeaderList
