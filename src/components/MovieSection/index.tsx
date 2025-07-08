b60036c5da086756f712a67822117985537275dd
J3eff <jeffersonvideo125@gmail.com>
2025-07-08 16:07:01 -0300

    Isolando responsabilidades

--- src/components/MovieSection/index.tsx
+++ src/components/MovieSection/index.tsx
@@ -5,21 +5,30 @@ import InputText from "../InputText";
 import styles from "./MovieSection.module.css";
 import MovieList from "../MovieList";
 import useFetchMovies from "../../hooks/useFetchMovies";
+import useFilterMovies from "../../hooks/useFilterMovies";
 
 const MovieSection = () => {
   const { movies, isLoading, error } = useFetchMovies();
-  
+  const { searchTerm, headleSearch, setSearchTerm, filteredMovies } =
+    useFilterMovies(movies);
+
   return (
     <main>
       <section className={styles.container}>
         <Fieldset variant="secondary">
-          <InputText placeholder="Buscar filmes..." />
-          <Button variant="icon">
+          <InputText
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+            placeholder="Buscar filmes..."
+          />
+          <Button variant="icon" onClick={headleSearch}>
             <FaSearch />
           </Button>
         </Fieldset>
         <h1 className={styles.titulo}>Em cartaz</h1>
-        <MovieList movies={movies} />
+        {isLoading && <p>Carregando filmes...</p>}
+        {error && <p className={styles.error}>{error}</p>}
+        <MovieList movies={filteredMovies} />
       </section>
     </main>
   );
