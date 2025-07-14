2aca0212d69471db2ffa7860e85d4c1c716b4726
J3eff <jeffersonvideo125@gmail.com>
2025-07-14 12:52:24 -0300

    Seção de Newsletter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner";
 import Header from "./components/Header";
 import MovieSection from "./components/MovieSection";
+import Newslatter from "./components/Newsletter";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Header />
       <Banner src="./banner-desktop.png" alt="Banner" />
       <MovieSection />
+      <Banner src="./banner-combo-desktop.png" alt="Combo" />
+      <Newslatter />
     </>
   );
 }
